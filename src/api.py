bf795fe7a72cebd15cf5fc5cd92c67bde5f265f3
Alessia Laforgia <84467216+AlessiaLa@users.noreply.github.com>
2021-11-09 17:59:32 +0100

    Start of video api in api.py

--- src/api.py
+++ src/api.py
@@ -10,7 +10,7 @@ from src.run import run_net
 
 app = FastAPI()
 
-@app.post("/predictions/num", responses={200: {"description": "returns the predicted number of people in the image", "content": {"json": {"example": {"image_name": "image.jpg", "people_number": "120"}}}}})
+@app.post("/predictions/imgnum", responses={200: {"description": "returns the predicted number of people in the image", "content": {"json": {"example": {"image_name": "image.jpg", "people_number": "120"}}}}})
 async def predictPeopleNumber(file: UploadFile = File(...)):
     with open(f'{file.filename}','wb') as buffer:
         shutil.copyfileobj(file.file, buffer)
@@ -24,7 +24,7 @@ async def predictPeopleNumber(file: UploadFile = File(...)):
 
     return {"image_name": data['img_name'], "people_number": data['count']}
 
-@app.post("/predictions/heatmap", responses={200: {"description": "returns the people heatmap", "content": {"image/png": {"example": "No example available."}}}})
+@app.post("/predictions/imgheatmap", responses={200: {"description": "returns the people heatmap of an image", "content": {"image/png": {"example": "No example available."}}}})
 async def predictHeatmap(file: UploadFile = File(...), download: bool = False):
     with open(f'{file.filename}','wb') as buffer:
         shutil.copyfileobj(file.file, buffer)
@@ -41,11 +41,9 @@ async def predictHeatmap(file: UploadFile = File(...), download: bool = False):
             return FileResponse(path, media_type="image/png")
     return {"error":"File not found!" + str(path) }
 
-
-
     return {"image_name": data['img_name'], "people_number": data['count']}
 
-@app.post("/predictions", responses={200: {"description": "returns the people heatmap and the predicted people count in the headers", "content": {"image/png": {"example": "No example available"}}}})
+@app.post("/predictions/imgpreds", responses={200: {"description": "returns the people heatmap and the predicted people count in the headers for an image", "content": {"image/png": {"example": "No example available"}}}})
 async def predictAll(file: UploadFile = File(...)):
     with open(f'{file.filename}','wb') as buffer:
         shutil.copyfileobj(file.file, buffer)
@@ -64,5 +62,60 @@ async def predictAll(file: UploadFile = File(...)):
         return FileResponse(path, headers=results, media_type="image/png")
     return {"error": "File not found!" + str(path)}
 
+### VIDEO API ###
+
+@app.post("/predictions/videonum", responses={200: {"description": "returns the predicted number of people in the video", "content": {"json": {"example": {"video_name": "video.mp4", "people_number": "120"}}}}})
+async def predictVideoPeopleNumber(file: UploadFile = File(...)):
+    with open(f'{file.filename}','wb') as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    run_net(file.filename,['count_callback'])
+
+    with open('count_results.json') as f:
+        data = json.load(f)
+
+    os.remove(file.filename)
+
+    return {"video_name": data['video_name'], "people_number": data['count']}
 
 
+
+@app.post("/predictions/videoheatmap", responses={200: {"description": "returns the people heatmap of a video", "content": {"video/mp4": {"example": "No example available."}}}})
+async def predictVideoHeatmap(file: UploadFile = File(...), download: bool = False):
+    with open(f'{file.filename}','wb') as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    run_net(file.filename,['save_callback'])
+
+    os.remove(file.filename)
+
+    path = "prediction.mp4"
+    if os.path.exists(path):
+        if download:
+            return FileResponse(path, media_type="video/mp4", filename="heatmap.mp4")
+        else:
+            return FileResponse(path, media_type="video/mp4")
+    return {"error":"File not found!" + str(path) }
+
+    return {"video_name": data['video_name'], "people_number": data['count']}
+
+
+
+@app.post("/predictions/videopreds", responses={200: {"description": "returns the people heatmap and the predicted people count in the headers for a video", "content": {"video/mp4": {"example": "No example available"}}}})
+async def predictVideoAll(file: UploadFile = File(...)):
+    with open(f'{file.filename}','wb') as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    run_net(file.filename,['count_callback','save_callback'])
+
+    os.remove(file.filename)
+
+    with open('../count_results.json') as f:
+        data = json.load(f)
+
+    results = {"video_name": data['video_name'], "people_number": data['count']}
+
+    path = "prediction.mp4"
+    if os.path.exists(path):
+        return FileResponse(path, headers=results, media_type="video/mp4")
+    return {"error": "File not found!" + str(path)}
