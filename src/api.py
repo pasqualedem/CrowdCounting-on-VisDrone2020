a3d089ba48a78e93879231aeb93a92e4c57e634f
Pasquale De Marinis <pasquale199876@hotmail.it>
2021-11-18 15:34:53 +0100

    Fixed merge issues

--- src/api.py
+++ src/api.py
@@ -14,7 +14,6 @@ import glob
 import ffmpeg
 from zipfile import ZipFile
 
-
 description = """Drone-CrowdCounting API allows you to deal with crowd air view pictures shot with drones
 
 ## Users
@@ -91,8 +90,8 @@ async def predictFromImages(file: UploadFile = File(...), count: bool = True, he
         os.makedirs(tmp)
     else:
         for f in os.listdir(tmp):
-            #print(f)
-            os.remove(os.path.join(tmp,f))
+            # print(f)
+            os.remove(os.path.join(tmp, f))
 
     if count and not heatmap:
         run_net(file.filename, ['count_callback'], model)
@@ -105,14 +104,12 @@ async def predictFromImages(file: UploadFile = File(...), count: bool = True, he
 
     if heatmap and not count:
         run_net(file.filename, ['save_callback'], model)
-        run_net(file.filename, ['save_callback'])
         path = os.path.join(tmp, Path(file.filename).stem + '.png')
         os.remove(file.filename)
         return FileResponse(path, media_type="image/png")
 
     if heatmap and count:
         run_net(file.filename, ['count_callback', 'save_callback'], model)
-        run_net(file.filename, ['count_callback', 'save_callback'])
         path = os.path.join(tmp, Path(file.filename).stem + '.png')
         os.remove(file.filename)
         with open('count_results.json') as f:
@@ -138,28 +135,23 @@ async def predictFromImages(file: UploadFile = File(...), count: bool = True, he
             "content": {
                 "application/json": {
                     "example": {
-                        "# to be defined "
-
-            "content" : {
-                "application/json" : {
-                    "example" : {
                         "detail": [
-                        {"image_name":"0","people_number":"317.0"},
-                        {"image_name":"1","people_number":"303.0"},
-                        {"image_name":"2","people_number":"306.0"},
-                        {"image_name":"3","people_number":"306.0"},
-                        {"image_name":"4","people_number":"308.0"},
-                        {"image_name":"5","people_number":"311.0"},
-                        {"image_name":"6","people_number":"324.0"},
-                        {"image_name":"7","people_number":"316.0"},
-                        {"image_name":"8","people_number":"314.0"},
-                        {"image_name":"9","people_number":"305.0"},
-                        {"image_name":"10","people_number":"310.0"},
-                        {"image_name":"11","people_number":"307.0"},
-                        {"image_name":"12","people_number":"314.0"},
-                        {"image_name":"13","people_number":"304.0"},
-                        {"image_name":"14","people_number":"304.0"}
-                       ]
+                            {"image_name": "0", "people_number": "317.0"},
+                            {"image_name": "1", "people_number": "303.0"},
+                            {"image_name": "2", "people_number": "306.0"},
+                            {"image_name": "3", "people_number": "306.0"},
+                            {"image_name": "4", "people_number": "308.0"},
+                            {"image_name": "5", "people_number": "311.0"},
+                            {"image_name": "6", "people_number": "324.0"},
+                            {"image_name": "7", "people_number": "316.0"},
+                            {"image_name": "8", "people_number": "314.0"},
+                            {"image_name": "9", "people_number": "305.0"},
+                            {"image_name": "10", "people_number": "310.0"},
+                            {"image_name": "11", "people_number": "307.0"},
+                            {"image_name": "12", "people_number": "314.0"},
+                            {"image_name": "13", "people_number": "304.0"},
+                            {"image_name": "14", "people_number": "304.0"}
+                        ]
                     }
                 }
             }
@@ -172,7 +164,6 @@ async def predictFromImages(file: UploadFile = File(...), count: bool = True, he
                         "detail": "Why predict something and not wanting any result?"
                     }
                 }
-
             }
         }
     })
@@ -185,8 +176,8 @@ async def predictFromVideos(file: UploadFile = File(...), count: bool = True, he
         os.makedirs(tmp)
     else:
         for f in os.listdir(tmp):
-            #print(f)
-            os.remove(os.path.join(tmp,f))
+            # print(f)
+            os.remove(os.path.join(tmp, f))
 
     if count and not heatmap:
         run_net(file.filename, ['count_callback'], model)
@@ -200,43 +191,16 @@ async def predictFromVideos(file: UploadFile = File(...), count: bool = True, he
         return results
 
     if heatmap and not count:
-        run_net(file.filename, ['save_callback'])
-        file_name = Path(file.filename).stem + '_heatmap{}'.format(os.path.splitext(file.filename)[1])
-        file_path = os.path.join(tmp, file_name)
-        list_pred = os.path.join(tmp, 'list.txt')
-        cap = cv2.VideoCapture(file.filename)
         run_net(file.filename, ['save_callback'], model)
-        os.remove(file.filename)
-
-        # generate file list for heatmaps
-        f = open(list_pred, "w")
-        for i in range(len(os.listdir(tmp))-1):
-          f.write('file {}.png \n'.format(i))
-        f.close()
-
-        fps = cap.get(cv2.CAP_PROP_FPS)
-
-        # generate a video from the heatmaps obtained from each frame
-        (
-            ffmpeg
-            .input(list_pred, r=fps, f='concat', safe='0')
-            .output(file_path)
-            .run()
-        )
-        return FileResponse(file_path, media_type="video/mp4", filename=file_name)
-
-    if heatmap and count:
-        run_net(file.filename, ['count_callback','save_callback'])
         file_name = Path(file.filename).stem + '_heatmap{}'.format(os.path.splitext(file.filename)[1])
         file_path = os.path.join(tmp, file_name)
         list_pred = os.path.join(tmp, 'list.txt')
         cap = cv2.VideoCapture(file.filename)
-        run_net(file.filename, ['count_callback', 'save_callback'], model)
         os.remove(file.filename)
 
         # generate file list for heatmaps
         f = open(list_pred, "w")
-        for i in range(len(os.listdir(tmp))-1):
+        for i in range(len(os.listdir(tmp)) - 1):
             f.write('file {}.png \n'.format(i))
         f.close()
 
@@ -245,9 +209,34 @@ async def predictFromVideos(file: UploadFile = File(...), count: bool = True, he
         # generate a video from the heatmaps obtained from each frame
         (
             ffmpeg
-            .input(list_pred, r=fps, f='concat', safe='0')
-            .output(file_path)
-            .run()
+                .input(list_pred, r=fps, f='concat', safe='0')
+                .output(file_path)
+                .run()
+        )
+        return FileResponse(file_path, media_type="video/mp4", filename=file_name)
+
+    if heatmap and count:
+        run_net(file.filename, ['count_callback', 'save_callback'], model)
+        file_name = Path(file.filename).stem + '_heatmap{}'.format(os.path.splitext(file.filename)[1])
+        file_path = os.path.join(tmp, file_name)
+        list_pred = os.path.join(tmp, 'list.txt')
+        cap = cv2.VideoCapture(file.filename)
+        os.remove(file.filename)
+
+        # generate file list for heatmaps
+        f = open(list_pred, "w")
+        for i in range(len(os.listdir(tmp)) - 1):
+            f.write('file {}.png \n'.format(i))
+        f.close()
+
+        fps = cap.get(cv2.CAP_PROP_FPS)
+
+        # generate a video from the heatmaps obtained from each frame
+        (
+            ffmpeg
+                .input(list_pred, r=fps, f='concat', safe='0')
+                .output(file_path)
+                .run()
         )
 
         count_file = '{}_people_count.json'.format(Path(file_path).stem)
@@ -267,11 +256,11 @@ async def predictFromVideos(file: UploadFile = File(...), count: bool = True, he
         zipfilepath = zipfilepath + '.zip'
         os.remove(count_file)
 
-        return FileResponse(zipfilepath, media_type="application/x-zip-compressed", filename= zipfilename)
+        return FileResponse(zipfilepath, media_type="application/x-zip-compressed", filename=zipfilename)
 
     if not count and not heatmap:
         raise HTTPException(status_code=404, detail="Why predict something and not wanting any result?")
 
 
 if __name__ == '__main__':
-    uvicorn.run("api:app", host="0.0.0.0",  reload=True, reload_dirs=['src'])
+    uvicorn.run("api:app", host="0.0.0.0", reload=True, reload_dirs=['src'])
