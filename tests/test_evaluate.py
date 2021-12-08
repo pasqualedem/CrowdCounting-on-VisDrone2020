6b645c50de09cb8be48f72b804b966f5facb46b4
Pasquale De Marinis <pasquale199876@hotmail.it>
2021-12-08 21:52:08 +0100

    Added tests for train and evaluate

--- tests/test_evaluate.py
+++ tests/test_evaluate.py
@@ -1,11 +1,27 @@
+import os
 from tempfile import NamedTemporaryFile
+
+import torch.cuda
+
 from models.CC import CrowdCounter
 from config import cfg
-from dataset.visdrone import cfg_data
+from dataset.visdrone import cfg_data, make_dataframe, VisDroneDataset
+from sklearn.metrics import mean_absolute_error
 
 import evaluate as eval
 import pytest
 import json
+
+
+def load_test_subset():
+    """
+    Create a VisDroneDataset object in test mode with 3 batches
+    @return: the visdrone testset
+    """
+    df = make_dataframe(os.path.join(cfg_data.DATA_PATH, cfg_data.DATA_SUBFOLDER, 'test'), cfg_data.SIZE)
+    df = df[0:cfg.VAL_BATCH_SIZE * 3]
+    ds = VisDroneDataset(df, train=cfg.TRAIN, gt_transform=cfg.GT_TRANSFORM)
+    return ds
 
 
 @pytest.mark.evaluate
@@ -40,3 +56,16 @@ class TestPreliminarTest:
         cfg.PRE_TRAINED = None
         model = eval.load_CC_test()
         assert type(model) == CrowdCounter
+
+    @pytest.mark.evaluate
+    @pytest.mark.slow
+    def test_evaluate(self):
+        losses = {'mae': mean_absolute_error}
+        cfg.PRE_TRAINED = None
+        device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        cfg.GPU = device
+        metrics = eval.evaluate_model(eval.load_CC_test, load_test_subset, cfg.TEST_BATCH_SIZE, cfg.N_WORKERS, losses,
+                                      device,
+                                      None)
+        assert len(metrics) == 1
+        assert metrics.get('mae') >= 0
--- tests/test_train.py
+++ tests/test_train.py
@@ -1,10 +1,44 @@
-from train import load_CC_train, load_yaml_train_params, initialize_dynamic_params
+import os
+
+import sklearn
+import torch
+
+from train import load_CC_train, load_yaml_train_params, initialize_dynamic_params, Trainer
+from dataset.visdrone import load_train_val, cfg_data, make_dataframe, VisDroneDataset
 from config import cfg
 from models.CC import CrowdCounter
-from dataset.visdrone import cfg_data
 from easydict import EasyDict
+import tempfile
 
 import pytest
+import torch.utils.data as data_utils
+
+
+def load_train_subset():
+    """
+     Create a train and validation DataLoader from the specified folder that is a subset with 3 batches
+     config values are used (VAL_SIZE, VAL_BATCH_SIZE, N_WORKERS)
+
+     @return: the train and validation DataLoader
+     """
+    df = make_dataframe(os.path.join(cfg_data.DATA_PATH, cfg_data.DATA_SUBFOLDER, 'train'), cfg_data.SIZE)
+    # Split the dataframe in train and validation
+    train_df, valid_df = sklearn.model_selection.train_test_split(
+        df, test_size=cfg.VAL_SIZE, shuffle=True
+    )
+    train_df = train_df.reset_index(drop=True)
+    valid_df = valid_df.reset_index(drop=True)
+    train_df = train_df[0:cfg.TRAIN_BATCH_SIZE * 3]
+    valid_df = valid_df[0:cfg.VAL_BATCH_SIZE * 3]
+
+    train_set = VisDroneDataset(train_df)
+    train_loader = torch.utils.data.DataLoader(
+        train_set, batch_size=cfg.TRAIN_BATCH_SIZE, num_workers=cfg.TRAIN_BATCH_SIZE, shuffle=True)
+
+    val_set = VisDroneDataset(valid_df)
+    val_loader = torch.utils.data.DataLoader(
+        val_set, batch_size=cfg.VAL_BATCH_SIZE, num_workers=cfg.VAL_BATCH_SIZE, shuffle=True)
+    return train_loader, val_loader
 
 
 class TestPreliminarTrain:
@@ -31,3 +65,23 @@ class TestPreliminarTrain:
     def test_load_CC_train(self):
         model = load_CC_train()
         assert type(model) == CrowdCounter
+
+    @pytest.mark.train
+    @pytest.mark.slow
+    @pytest.mark.cuda
+    def test_train(self):
+        cfg.update(self.train_params)
+        cfg_data.update(self.global_params)
+        initialize_dynamic_params()
+        cfg.PRETRAINED = None
+        cfg.MAX_EPOCH = 1
+        cfg.EXP_PATH = tempfile.mkdtemp()
+        cfg.PRINT_FREQ = 1
+
+        trainer = Trainer(dataloader=load_train_subset,
+                          cfg_data=cfg_data,
+                          net_fun=load_CC_train
+                          )
+        trainer.train()
+        assert os.path.exists(os.path.join(cfg.EXP_PATH, 'model.pth'))
+        assert os.path.exists(os.path.join(cfg.EXP_PATH, 'log.txt'))
