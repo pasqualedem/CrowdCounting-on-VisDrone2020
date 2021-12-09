0d5a81bf66aa7acde0f2f3d78a21dbfe48ab3714
Pasquale De Marinis <pasquale199876@hotmail.it>
2021-12-09 12:06:26 +0100

    Fix tests

--- tests/test_ml.py
+++ tests/test_ml.py
@@ -1,10 +1,11 @@
 import json
+import pytest
 
 MIN_METRICS = {
     'rmse': 45.0,
     'mae': 35.0}
 
-
+@pytest.mark.ml
 def test_metrics():
     with open('metrics.json') as metrics_file:
         metrics = json.load(metrics_file)
