68dc0840a1e0144243551b6750c1f6814c3e71cd
Pasquale De Marinis <pasquale199876@hotmail.it>
2022-01-10 19:41:02 +0100

    Fixed metrics tests

--- tests/test_ml.py
+++ tests/test_ml.py
@@ -1,14 +1,30 @@
 import json
 import pytest
+import os
 
 MIN_METRICS = {
     'rmse': 45.0,
     'mae': 35.0}
 
+METRICS_FILE = 'metrics.json'
+GIT_METRICS_FILE = 'https://raw.githubusercontent.com/se4ai2122-cs-uniba/Drone-CrowdCounting/main/metrics.json'
+DEPLOYED_METRICS_FILE = 'deployed_metrics.json'
+
+
 @pytest.mark.ml
 def test_metrics():
-    with open('metrics.json') as metrics_file:
+    with open(METRICS_FILE) as metrics_file:
         metrics = json.load(metrics_file)
 
     for metric in MIN_METRICS:
         assert MIN_METRICS[metric] > metrics[metric]
+
+    os.system('wget {} -O {}'.format(GIT_METRICS_FILE, DEPLOYED_METRICS_FILE))
+
+    if os.path.exists(DEPLOYED_METRICS_FILE):
+        with open(DEPLOYED_METRICS_FILE) as metrics_file:
+            deployed_metrics = json.load(metrics_file)
+
+        for metric in MIN_METRICS:
+            assert metrics[metric] >= deployed_metrics[metric]
+
